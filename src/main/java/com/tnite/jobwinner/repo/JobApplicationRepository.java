f5b9678592eedc690901cb45ed3444023c3147db
Britgray <britgray2000@gmail.com>
2024-02-01 03:31:26 -0500

    added backend

--- src/main/java/com/tnite/jobwinner/repo/JobApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnite.jobwinner.repo;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import com.tnite.jobwinner.model.JobApplication;
-
-public interface JobApplicationRepository extends ReactiveCrudRepository<JobApplication, Integer>{
-
-}
--- src/main/java/com/tnite/jobwinner/repo/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnite.jobwinner.repo;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import com.tnite.jobwinner.model.Profile;
-
-public interface ProfileRepository extends ReactiveCrudRepository<Profile, Integer>{
-
-}
